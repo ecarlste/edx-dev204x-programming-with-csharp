bed3ac5dc2f51c063b08262a2e59a83bd3f7b7ae
Erik S. Carlsten <ecarlste@gmail.com>
2015-04-16 16:29:36 -0500

    Added struct for holding student information

--- module-4-assignment/UniversityInfoUsingStructs.cs
+++ module-4-assignment/UniversityInfoUsingStructs.cs
@@ -4,6 +4,26 @@ namespace ModuleFourAssignment
 {
     class UniversityInfoUsingStructs
     {
+        struct Student
+        {
+            internal string firstName;
+            internal string lastName;
+
+            internal DateTime birthdate;
+
+            internal string addressLine1;
+            internal string addressLine2;
+            internal string city;
+            internal string stateOrProvince;
+            internal string zipOrPostalCode;
+            internal string country;
+
+            internal Decimal accountBalance;
+            internal float overallGPA;
+            internal char gender;
+            internal bool isEnrolled;
+        }
+
         static void Main(string[] args)
         {
         }
