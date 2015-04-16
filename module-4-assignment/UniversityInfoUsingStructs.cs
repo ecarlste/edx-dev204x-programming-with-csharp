c9838615266059e77d51c1a2cc78fe82f883a527
Erik S. Carlsten <ecarlste@gmail.com>
2015-04-16 17:06:34 -0500

    Added Student constructor and method to create/populate a student array

--- module-4-assignment/UniversityInfoUsingStructs.cs
+++ module-4-assignment/UniversityInfoUsingStructs.cs
@@ -22,10 +22,48 @@ namespace ModuleFourAssignment
             internal float overallGPA;
             internal char gender;
             internal bool isEnrolled;
+
+            public Student(string firstName, string lastName, DateTime birthdate, string addressLine1,
+                string addressLine2, string city, string stateOrProvince, string zipOrPostalCode, string country,
+                Decimal accountBalance, float overallGPA, char gender, bool isEnrolled)
+            {
+                this.firstName = firstName;
+                this.lastName = lastName;
+
+                this.birthdate = birthdate;
+
+                this.addressLine1 = addressLine1;
+                this.addressLine2 = addressLine2;
+                this.city = city;
+                this.stateOrProvince = stateOrProvince;
+                this.zipOrPostalCode = zipOrPostalCode;
+                this.country = country;
+
+                this.accountBalance = accountBalance;
+                this.overallGPA = overallGPA;
+                this.gender = gender;
+                this.isEnrolled = isEnrolled;
+            }
         }
 
         static void Main(string[] args)
         {
+            Student[] students = CreateStudentArrayAndPopulateOneEntry();
+
+            Console.WriteLine(students[0].firstName);
+        }
+
+        private static Student[] CreateStudentArrayAndPopulateOneEntry()
+        {
+            Student[] students = new Student[5];
+
+            students[0] = new Student
+                (
+                    "Fred", "Flintstone", new DateTime(1941, 7, 3), "417 Special Bvd", "", "Boulder", "Montana",
+                    "55542", "US", 217.31m, 3.29f, 'M', false
+                );
+
+            return students;
         }
     }
 }
