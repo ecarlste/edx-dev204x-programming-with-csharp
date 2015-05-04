8c0dbf226f5eb02bfa3e6652b1923a51286b5dfd
Erik S. Carlsten <ecarlste@gmail.com>
2015-05-04 18:42:48 -0500

    Changed UI layout of ListView and switched to ListBox control

--- CSharpFileMergeTool/MainWindow.xaml.cs
+++ CSharpFileMergeTool/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,24 +22,63 @@ namespace CSharpFileMergeTool
     /// </summary>
     public partial class MainWindow : Window
     {
+        ObservableCollection<FileInfo> fileList = new ObservableCollection<FileInfo>();
+        public ObservableCollection<FileInfo> FileList
+        {
+            get { return fileList; }
+            set { fileList = value; }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            // fileNamesListView.ItemsSource = fileList;
         }
 
         private void fileNamesTextBlock_Drop(object sender, DragEventArgs e)
         {
             string[] fileNames = (string[])e.Data.GetData(DataFormats.FileDrop, true);
-            TextBlock textBlock = sender as TextBlock;
-
-            StringBuilder sb = new StringBuilder();
+            ListView listView = sender as ListView;
 
             foreach (string fileName in fileNames)
             {
-                sb.Append(fileName + Environment.NewLine);
+                fileList.Add(new FileInfo(fileName));
             }
 
-            textBlock.Text = sb.ToString();
+            e.Handled = true;
+        }
+    }
+
+    public class FileInfo
+    {
+        string path;
+        public string Path
+        {
+            get { return path; }
+        }
+
+        string name;
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public FileInfo(string pathName)
+        {
+            string[] pathComponents = pathName.Split('\\');
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < pathComponents.Length - 1; i++)
+            {
+                if (i > 0)
+                {
+                    sb.Append('\\');
+                }
+                sb.Append(pathComponents[i]);
+            }
+
+            path = sb.ToString();
+            name = pathComponents[pathComponents.Length - 1];
         }
     }
 }
