f8f4ac4a3565dce13a32cb4ddd87a0c5ee2b8304
Erik S. Carlsten <ecarlste@gmail.com>
2015-05-04 19:20:57 -0500

    Moved FileInfo into its own class

--- CSharpFileMergeTool/MainWindow.xaml.cs
+++ CSharpFileMergeTool/MainWindow.xaml.cs
@@ -32,7 +32,6 @@ namespace CSharpFileMergeTool
         public MainWindow()
         {
             InitializeComponent();
-            // fileNamesListView.ItemsSource = fileList;
         }
 
         private void fileNamesTextBlock_Drop(object sender, DragEventArgs e)
@@ -56,64 +55,6 @@ namespace CSharpFileMergeTool
             {
                 fileList.Add(fileInfo);
             }
-        }
-    }
-
-    public class FileInfo : IComparable
-    {
-        string path;
-        public string Path
-        {
-            get { return path; }
-        }
-
-        string name;
-        public string Name
-        {
-            get { return name; }
-        }
-
-        public string FullPathName
-        {
-            get { return path + name; }
-        }
-
-        public FileInfo(string pathName)
-        {
-            string[] pathComponents = pathName.Split('\\');
-            StringBuilder sb = new StringBuilder();
-
-            for (int i = 0; i < pathComponents.Length - 1; i++)
-            {
-                if (i > 0)
-                {
-                    sb.Append('\\');
-                }
-                sb.Append(pathComponents[i]);
-            }
-
-            path = sb.ToString();
-            name = pathComponents[pathComponents.Length - 1];
-        }
-
-        public int CompareTo(object obj)
-        {
-            if (obj == null) return 1;
-
-            FileInfo otherFileInfo = obj as FileInfo;
-            if (otherFileInfo != null)
-            {
-                if (name.Equals(otherFileInfo.Name))
-                {
-                    return FullPathName.CompareTo(otherFileInfo.FullPathName);
-                }
-                else
-                {
-                    return name.CompareTo(otherFileInfo.Name);
-                }
-            }
-            else
-                throw new ArgumentException("Object is not a FileInfo");
         }
     }
 }
